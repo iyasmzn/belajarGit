cd8badf5b8e6fa5f8a66e7fe12a279cd1d5c5288
iyasmzn <iyasmzn07@gmail.com>
2019-12-09 09:55:20 +0700

    cleaned index

--- index.php
+++ index.php
@@ -39,9 +39,7 @@
         <small>it all starts here</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
+        <li class="active"><i class="fa fa-home">Dashboard</i></li>
       </ol>
     </section>
 
